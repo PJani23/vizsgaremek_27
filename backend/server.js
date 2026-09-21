c3fc531b79e771913ac4122095beb706532bd9e9
EgervMihaly <158279272+EgervMihaly@users.noreply.github.com>
2026-09-21 09:19:27 +0200

    script

--- backend/server.js
+++ backend/server.js
@@ -1,2 +1,58 @@
+const json = JSON.parse(
+[
+  {
+    "nev": "Rántotta",
+    "kep": "images/rantotta.jpg",
+    "ido": 10,
+    "nehezseg": "könnyű",
+    "adag": 1,
+    "hozzavalok": ["tojás", "só", "bors", "olaj"],
+    "lepesek": [
+      "A tojásokat egy tálban felverjük.",
+      "Sózzuk, borsozzuk.",
+      "Olajat hevítünk a serpenyőben.",
+      "A tojást beleöntjük és folyamatosan keverjük.",
+      "3-4 perc alatt készre sütjük."
+    ],
+    "tulajdonsag": [
+      "egyszerű"
+    ]
+  },
+  {
+    "nev": "Palacsinta",
+    "kep": "images/palacsinta.jpg",
+    "ido": 25,
+    "nehezseg": "közepes",
+    "adag": 4,
+    "hozzavalok": ["liszt", "tojás", "tej", "cukor", "olaj", "só"],
+    "lepesek": [
+      "A lisztet, tojást, tejet és cukrot összekeverjük.",
+      "Hozzáadunk egy csipet sót.",
+      "A tésztát 10 percig pihentetjük.",
+      "Olajjal kikent serpenyőben kisütjük a palacsintákat."
+    ],
+    "tulajdonsag": [
+      "egyszerű"
+    ]
+  }
+])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
